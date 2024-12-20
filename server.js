5103937f101ac0912fbd346565c8c491fd74262c
kiyaanmarketing <Rajy4251@gmail.com>
2024-12-20 15:56:00 +0000

    port change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(corsMiddleware);
 app.use(bodyParser.json());
