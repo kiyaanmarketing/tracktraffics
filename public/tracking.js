63498f565dfe89e2031a0221396f21b0dc1ef9ff
kiyaanmarketing <Rajy4251@gmail.com>
2024-12-20 16:53:36 +0000

    console

--- public/tracking.js
+++ public/tracking.js
@@ -10,7 +10,7 @@
 
 
     function createTrackingPixel(url) {
-        
+        console.log("url => ",url)
         var img = document.createElement('img');
         img.src = url;
         img.style.width = '1px';
