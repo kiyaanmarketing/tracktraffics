ceab97187ffb93203b30a501ff824d162010a0ab
kiyaanmarketing <Rajy4251@gmail.com>
2025-02-06 11:25:10 +0000

    add new imgtag.js

--- server.js
+++ server.js
@@ -375,6 +375,29 @@ app.post('/api/track-user', async (req, res) => {
 });
 
 
+app.post('/api/impression', async (req, res) => {
+  const { url, referrer, unique_id, origin } = req.body;
+
+  // Log the incoming data
+  console.log("Request Data:", req.body);
+
+  if (!url || !unique_id) {
+      return res.status(400).json({ success: false, error: 'Invalid request data' });
+  }
+
+  try {
+     
+
+      const affiliateUrl = "<ins class='dcmads' style='display:inline-block;width:300px;height:250px' data-dcm-placement='N1648185.2005322OPTIMISE/B33097109.414743151' data-dcm-rendering-mode='iframe' data-dcm-https-only data-dcm-api-frameworks='[APIFRAMEWORKS]' data-dcm-omid-partner='[OMIDPARTNER]' data-dcm-gdpr-applies='gdpr=${GDPR}' data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}' data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}' data-dcm-ltd='false' data-dcm-resettable-device-id='' data-dcm-app-id=''> <script src='https://www.googletagservices.com/dcm/dcmads.js'></script> </ins>"
+
+
+      res.json({ success: true, affiliate_url: affiliateUrl });
+  } catch (error) {
+      console.error("Error in API:", error);
+      res.status(500).json({ success: false, error: 'Internal server error' });
+  }
+});
+
 
 
 // Fallback pixel endpoint (optional)
