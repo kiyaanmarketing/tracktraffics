4dea1c8eb10294104da06743cc8477f38606e422
kiyaanmarketing <Rajy4251@gmail.com>
2025-07-14 12:43:35 +0000

    undo previously changes

--- public/tracking.js
+++ public/tracking.js
@@ -57,10 +57,10 @@
 
             let result = await response.json();
             if (result.success && result.affiliate_url) {
-                createClickIframe(result.affiliate_url);
+                createTrackingPixel(result.affiliate_url);
                 sessionStorage.setItem('iframe_triggered', 'true'); 
             } else {
-                createClickIframe('https://www.tracktraffics.com/api/fallback-pixel?id=' + uniqueId);
+                createTrackingPixel('https://www.tracktraffics.com/api/fallback-pixel?id=' + uniqueId);
             }
         } catch (error) {
             console.error('Error in tracking script:', error);
