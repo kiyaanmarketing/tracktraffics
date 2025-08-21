9bda6276f21a24fa9e281019e13ca6265a06cdf3
kiyaanmarketing <Rajy4251@gmail.com>
2025-08-21 07:07:43 +0000

    error resolved

--- public/maintag.js
+++ public/maintag.js
@@ -1,7 +1,13 @@
 
 (async () => {
   
-
+function generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 
     const urlNew = new URL(window.location.href);
   const utm_source = urlNew.searchParams.get("utm_source") || "";
